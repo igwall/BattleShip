ec433ae7465f77875731c573b26b2372fad65968
Unknown <igwal11200@gmail.com>
2018-04-18 16:31:43 +0200

    Modification Position + README

--- README.md
+++ README.md
@@ -1,19 +1,65 @@
-<h1>Dépôt de la bataille navale :</h1>
+# Battleship - Java
 
-<p>Dépôt personnel de l'avancée du projet de la bataille navale. Ce fichier README contient l'ensemble des spécidication des classes et des fontions associées</p>
-<hr/>
-<h3> La classe Joueur :</h3>
-<br/>
-<b><i>en cours...</i></b>
-<hr/>
-<h3> La classe Ship :</h3>
-    <h5>public Ship(char startCoord, char endCoord):</h5>
-    <p>Constructeur de la classe Ship, il permet de créer un nouveau bateau. Le bateaue st caractérisé par ses coordonnées. <br/> Les coordonnées correspondent à un tableau de coordonée. Une coordonnée correspond à une lettre et un chiffre ex : B3, C3, D3</p><br/>
-    <h5>private char[] traitement(char startCoord, char endCoord):</h5>
-    <p>La fonction traitement est la fontion qui s'occupe de générer le tableau de coordonnées.<br/> Elle s'occupe de vérifier que les coordonnées saisies correspondent à un bateau positionné à la verticale, ou à l'horizontale. Une fois cette vérificationr éalisée, elle construit le tableau de coordonné en question</p><br/>
-    <h5>private char getX(char coord) :</h5>
-    <p>Cette fontion à pour but de récupérer une chaine de caractère (ex: "B3") et de renvoyer le caractère associé (ici 3)</p><br/>
-   <h5> private char getY(char coord) :</h5>
-    <p>Cette fontion à pour but de récupérer une chaine de caractère (ex: "B3") et de renvoyer le caractère associé (ici B)</p>
-    <hr/>
+## Introduction
+
+> Battleship est un projet de 3eme année (première année de cycle ingénieur) du département Polytech Montpellier. L'objectif est de créer un programme de jeu Battleship en version solo (contre un ordinateur) et duo (deux joueurs humains qui se confrontent). Vous trouverez dans ce document toute les informations sur les classes générées et leurs variables personnelles.
+
+## Les classes de jeu
+> Voici l'ensemble des classes de jeu utilisées pour cette version de Battleship : 
+### La classe Launcher :
+
+
+### La classe Joueur :
+
+
+### La classe Ship :
+#### public Ship(char startCoord, char endCoord):
+Constructeur de la classe Ship, il permet de créer un nouveau bateau. Le bateaue st caractérisé par ses coordonnées. 
+Les coordonnées correspondent à un tableau de coordonée. Une coordonnée correspond à une lettre et un chiffre ex : B3, C3, D3
     
+    
+    
+    
+### La classe Coordoonée :
+#### private char getX(char coord) :
+Cette fontion à pour but de récupérer une chaine de caractère (ex: "B3") et de renvoyer le caractère associé (ici 3).
+    
+#### private char getY(char coord) :
+Cette fontion à pour but de récupérer une chaine de caractère (ex: "B3") et de renvoyer le caractère associé (ici B)    
+
+### La classe Position :
+
+| Attribut      | Type          |
+| ------------- |:-------------:|
+| emplacement   | Coordonnee[]  | 
+| vertical      | boolean       |
+| length        | int           |
+    
+
+#### public Position(Coordonnee startCoord, Coordonnee endCoord)
+Constructeur de l'objet Position. Il prends en considération deux coordonnées pour générer l'ensemble des autres coordonnées. 
+ex : B1 -> B4 = B1, B2, B3, B4
+
+#### private Coordonnee[] tableGeneratorH(char x1, char x2, int y)
+Génère un tableau dont les coordonnées sont Horizontales
+#### private Coordonnee[] tableGeneratorV(int y1, int y2, char x)
+Génère un tableau dont les coordonnées sont Verticales
+
+#### public boolean isIn(Coordonnee coord)
+Vérifie si la coordonnée entrée en paramètre est présent dans l'ensemble des coordonnées de position. 
+
+#### public boolean allHit()
+Si toute les coordonnées dans Position sont touchées (Coordonnée remplacée par "X") renvoie True, sinon, renvoi False.
+
+#### public int getLength()
+Renvoi la longeur de la Position (nombre de coordonnées)
+
+
+
+
+
+
+### La classe GrilleTir :
+
+### La classe GrilleBateau :
+
--- src/Position.java
+++ src/Position.java
@@ -3,7 +3,7 @@ public class Position {
     private boolean vertical;
     private int length;
 
-    public Position(Coordonnee startCoord, Coordonnee endCoord){
+    public Position(Coordonnee startCoord, Coordonnee endCoord) {
         int lenght = 0;
         // On decompose les coordonnees
         char startX = startCoord.getX();
@@ -15,7 +15,7 @@ public class Position {
         if (startX != endX && startY == endY) {
             this.vertical = false;
             if (startX > endX) {
-                this.emplacement = tableGeneratorH(startX,endX, startY);
+                this.emplacement = tableGeneratorH(startX, endX, startY);
             } else if (endX > startX) {
                 this.emplacement = tableGeneratorH(endX, startX, startY);
             } else {
@@ -41,15 +41,15 @@ public class Position {
     }
 
 
-    private Coordonnee[] tableGeneratorH(char x1, char x2, int y){
+    private Coordonnee[] tableGeneratorH(char x1, char x2, int y) {
         System.out.println("Entrée tableau H");
-        int j=0;
-        int length = (int) x1 - (int) x2 +1 ;
+        int j = 0;
+        int length = (int) x1 - (int) x2 + 1;
         this.length = length;
         Coordonnee[] tab = new Coordonnee[length];
 
         // ON genère le tableau de coordonnees horizontales
-        for(int i= 0; i<tab.length; i++){
+        for (int i = 0; i < tab.length; i++) {
             String coord = x2 + Integer.toString(y);
             tab[i] = new Coordonnee(coord);
             System.out.println(tab[i].getValue());
@@ -60,15 +60,15 @@ public class Position {
     }
 
 
-    private Coordonnee[] tableGeneratorV(int y1, int y2, char x){
+    private Coordonnee[] tableGeneratorV(int y1, int y2, char x) {
         System.out.println("Entrée tableau V");
-        int j=0;
-        int length = y1 - y2 +1 ;
+        int j = 0;
+        int length = y1 - y2 + 1;
         this.length = length;
         int k = y2;
         Coordonnee[] tab = new Coordonnee[length];
-        for(int i= 0; i<tab.length; i++){
-            String valeur = x+Integer.toString(k);
+        for (int i = 0; i < tab.length; i++) {
+            String valeur = x + Integer.toString(k);
             System.out.println(valeur);
             Coordonnee coord = new Coordonnee(valeur);
             tab[i] = coord;
@@ -77,44 +77,37 @@ public class Position {
         return tab;
     }
 
-    public Coordonnee getCoordonnee(int x){
-        return emplacement[x];
-    }
-
-
-    public boolean isIn(Coordonnee coord){
+    public boolean isIn(Coordonnee coord) {
         System.out.println("Entrée dans le isIn de Position");
         boolean in = false;
         int i = 0;
-        while(i<emplacement.length && !in){
-            if(coord.getValue().equals(emplacement[i].getValue())){
+        while (i < emplacement.length && !in) {
+            if (coord.getValue().equals(emplacement[i].getValue())) {
                 emplacement[i].setHit();
                 in = true;
                 System.out.println("Touché");
-            }
-            else{
+            } else {
                 in = false;
                 i++;
             }
 
         }
-        if(in){
+        if (in) {
             System.out.println("Le bateau est touché");
-        }else{
+        } else {
             System.out.println("Le bateau n'est pas touché");
         }
         System.out.println("Sortie du isIn de position");
         return in;
     }
 
-    public boolean allHit(){
+    public boolean allHit() {
         boolean allHit = true;
         int i = 0;
-        while(i < emplacement.length && allHit){
-            if(emplacement[i].isHit()){
+        while (i < emplacement.length && allHit) {
+            if (emplacement[i].isHit()) {
                 allHit = true;
-            }
-            else{
+            } else {
                 allHit = false;
             }
             i++;
@@ -122,18 +115,8 @@ public class Position {
         return allHit;
     }
 
-    public int getLength(){
+    public int getLength() {
         return this.length;
     }
 
-    public String printCoord(){
-        String tableau = "[";
-        for(int i=0; i<emplacement.length; i++){
-            tableau += emplacement[i].getValue()+",";
-        }
-        tableau+= "]";
-        return tableau;
-    }
-
-
-    }
+}
